c8cc44e567a8afc2295763e29f3887c47ce59ce4
Isaac <publicstaticclass@gmail.com>
2021-02-16 21:26:27 -0500

    Updated React template.

---
+++ src/js/App.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function App() {
+    return(
+        <p style={{textAlign: 'center', fontSize: 24}}>This is a meeting assistance app.</p>
+    )
+}
--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,5 @@
-import React from "react";
-import ReactDom from "react-dom";
+import React from 'react';
+import ReactDom from 'react-dom';
+import App from './App';
 
-ReactDom.render(<h>Welcome to our 395 project!</h>, document.getElementById('root'));
+ReactDom.render(<App />, document.getElementById('insert'));
