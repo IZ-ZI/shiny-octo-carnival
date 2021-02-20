67a06df5ff5eccc7be0de7b52095b0f30461ae94
Isaac <publicstaticclass@gmail.com>
2021-02-20 01:53:46 -0500

    Added a basic demo using AntDesign React Component. We'll be using AntDesign kit for our UI, so please install the required dependencies.

--- src/js/App.js
+++ src/js/App.js
@@ -1,7 +1,21 @@
 import React from 'react';
+import ReactDom from 'react-dom';
+import {Button, Alert} from 'antd';
+import "antd/dist/antd.css";
 
 export default function App() {
     return(
-        <p style={{textAlign: 'center', fontSize: 24}}>This is a meeting assistance app.</p>
+        <Button type="primary" onClick={welcome}>
+            Click Me!
+        </Button>
     )
+}
+
+function welcome() {
+    console.log("Welcome");
+    ReactDom.render(<Alert message="Hello Group Fifteen!" description="Welcome to your 395 Project!" type="success" banner closable afterClose={removeAlert}/>, document.getElementById('alert'));
+}
+
+function removeAlert() {
+    ReactDom.unmountComponentAtNode(document.getElementById('alert'));
 }
